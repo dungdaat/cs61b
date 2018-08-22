cf37997520f7fa39d44ce3110a807157dc9b7a5e
Dungda <117020138@link.cuhk.edu.cn>
2018-08-22 16:45:19 +0800

    i don't know what is happening

--- nbody/nBody.java
+++ nbody/nBody.java
@@ -1,1 +1,27 @@
-public class nBody 
+public class NBody {
+	public static double readRadius(String filename) {
+		In readR = new In(filename);
+		int First = readR.readInt();
+		double Radius = readR.readDouble();
+		return Radius;
+	}
+	public static Planet[] readPlanets(String filename) {
+		Planet[] planetList = new Planet[5];
+		In readP = new In(filename);
+		int First = readP.readInt();
+		double Radius = readP.readDouble();
+		int count = 0;
+		while (count !=5) {
+			double xxPos = readP.readDouble();
+			double yyPos = readP.readDouble();
+	 		double xxVel = readP.readDouble();
+			double yyVel = readP.readDouble();
+	 		double mass = readP.readDouble();
+	 		String imgFileName = readP.readString();
+	 		planetList[count] = new Planet(xxPos, yyPos, xxVel, 
+		yyVel, mass, imgFileName);
+	 		count += 1;
+		}
+		return planetList;
+	}
+}
